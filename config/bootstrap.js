56ce4c45526fac8fd10476f522211ac05798c241
sdunham <dunham.scott@gmail.com>
2014-10-12 15:48:56 -0700

    Finished about the app view, dynamically generating QR code for app address on homepage using node js os module

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -13,6 +13,10 @@ module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+
+  sails.os = require('os');
+
+
   var examplePoll = [
     {
         title: "Example: What do you want for lunch?",
